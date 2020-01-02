eb4d1b2b0dbd566d871bc1daee59c3d5a0f87484
Miljan <48087520+m-misovic@users.noreply.github.com>
2020-01-02 13:44:06 +0100

    code update

--- app.js
+++ app.js
@@ -1,119 +1,112 @@
-var model = (function () {
+var model = (function() {
+  var itemList = [];
 
-    var itemList = [];
+  return {
+    addItemToDB: function(itemToAdd) {
+      itemList.push(itemToAdd);
+      return itemToAdd;
+    },
 
-    return {
-        addItemToDB: function (itemToAdd) {
-            itemList.push(itemToAdd);
-            return itemToAdd
-        },
+    removeItemFromDB: function(itemsToRemove) {
+      var temp = itemList.filter(function(item, index) {
+        if (itemsToRemove.indexOf(index) == -1) return item;
+      });
+      itemList = temp;
+      return itemsToRemove;
+    },
 
-        removeItemFromDB: function (itemsToRemove) {
-            var temp = itemList.filter(function (item, index) {
-                if (itemsToRemove.indexOf(index) == -1)
-                    return item
-            })
-            itemList = temp;
-            return itemsToRemove;
-        },
-
-        printItemList: function () {
-            console.log("Item list: " + itemList)
-        }
+    printItemList: function() {
+      console.log("Item list: " + itemList);
     }
+  };
 })();
 
+var view = (function() {
+  var DOMstrings = {
+    addBtn: "#button1",
+    removeBtn: "#button2",
+    input: "#userInput",
+    list: "#myList",
+    checkbox: ".checkbox"
+  };
 
-var view = (function () {
+  return {
+    getInput: function() {
+      return document.querySelector(DOMstrings.input).value;
+    },
 
-    var DOMstrings = {
-        addBtn: '#button1',
-        removeBtn: '#button2',
-        input: '#userInput',
-        list: '#myList',
-        checkbox: ".checkbox"
-    };
+    lookForChecked: function() {
+      var box = document.querySelectorAll(DOMstrings.checkbox);
+      arrayOfIndex = [];
 
-    return {
-        getInput: function () {
-            return document.querySelector(DOMstrings.input).value;
-        },
+      for (i = 0; i < box.length; i++) {
+        if (box[i].checked === true) {
+          arrayOfIndex.push(i);
+        }
+      }
+      return arrayOfIndex;
+    },
 
-        lookForChecked: function () {
-            var box = document.querySelectorAll(DOMstrings.checkbox);
-            arrayOfIndex = []
-
-            for (i = 0; i < box.length; i++) {
-                if (box[i].checked === true) {
-                    arrayOfIndex.push(i);
-                }
-            }
-            return arrayOfIndex;
-        },
-
-        showItemOnPage: function (itemToShow) {
-            var li = document.createElement("li");
-            li.innerHTML = `
+    showItemOnPage: function(itemToShow) {
+      var li = document.createElement("li");
+      li.innerHTML = `
             <input type="checkbox" class="checkbox">
             <span class="crossout">${itemToShow}</span>`;
-            document.querySelector(DOMstrings.list).appendChild(li)
-        },
+      document.querySelector(DOMstrings.list).appendChild(li);
+    },
 
-        removeItemFromPage: function (ItemsToRemove) {
-            var list = document.querySelector(DOMstrings.list)
-            var box = document.querySelectorAll(DOMstrings.checkbox);
-            ItemsToRemove.map(function (i) {
-                list.removeChild(box[i].parentElement)
-            })
-        },
+    removeItemFromPage: function(ItemsToRemove) {
+      var list = document.querySelector(DOMstrings.list);
+      var box = document.querySelectorAll(DOMstrings.checkbox);
+      ItemsToRemove.map(function(i) {
+        list.removeChild(box[i].parentElement);
+      });
+    },
 
-        clearInputField: function () {
-            document.querySelector(DOMstrings.input).value = "";
-            document.querySelector(DOMstrings.input).focus();
-        },
+    clearInputField: function() {
+      document.querySelector(DOMstrings.input).value = "";
+      document.querySelector(DOMstrings.input).focus();
+    },
 
-        getDOMstrings: function () {
-            return DOMstrings;
-        },
+    getDOMstrings: function() {
+      return DOMstrings;
     }
+  };
 })();
 
+var controller = (function(dataStorage, userInterface) {
+  var setupEventListeners = function() {
+    var DOM = userInterface.getDOMstrings();
 
-var controller = (function (dataStorage, userInterface) {
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        addItem();
+      }
+    });
+    document.querySelector(DOM.addBtn).addEventListener("click", addItem);
+    document.querySelector(DOM.removeBtn).addEventListener("click", deleteItem);
+  };
 
-    var setupEventListeners = function () {
-        var DOM = userInterface.getDOMstrings();
+  var addItem = function() {
+    var input = userInterface.getInput();
+    var newItem = dataStorage.addItemToDB(input);
+    userInterface.showItemOnPage(newItem);
+    userInterface.clearInputField();
+    dataStorage.printItemList();
+  };
 
-        document.addEventListener('keypress', function (event) {
-            if (event.keyCode === 13 || event.which === 13) {
-                addItem();
-            }
-        });
-        document.querySelector(DOM.addBtn).addEventListener('click', addItem);
-        document.querySelector(DOM.removeBtn).addEventListener('click', deleteItem);
-    };
+  var deleteItem = function() {
+    var checkedItems = userInterface.lookForChecked();
+    var itemsRemovedFromDB = dataStorage.removeItemFromDB(checkedItems);
+    userInterface.removeItemFromPage(itemsRemovedFromDB);
+    dataStorage.printItemList();
+  };
 
-    var addItem = function () {
-        var input = userInterface.getInput();
-        var newItem = dataStorage.addItemToDB(input);
-        userInterface.showItemOnPage(newItem);
-        userInterface.clearInputField();
-        dataStorage.printItemList();
-    };
-
-    var deleteItem = function () {
-        var checkedItems = userInterface.lookForChecked()
-        var itemsRemovedFromDB = dataStorage.removeItemFromDB(checkedItems)
-        userInterface.removeItemFromPage(itemsRemovedFromDB);
-        dataStorage.printItemList();
-    };
-
-    return {
-        init: function () {
-            setupEventListeners();
-        }
-    };
-
+  return {
+    init: function() {
+      setupEventListeners();
+    }
+  };
 })(model, view);
 
 controller.init();
